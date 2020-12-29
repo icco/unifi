21fcbf48ef207253e61be2bc390cf0d38d0d35e8
Nat Welch <nat@natwelch.com>
2020-12-28 19:27:17 -0500

    new field

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -9,15 +9,42 @@ import (
 
 // GetClients returns the numbers of clients tracked by this unifi.
 func GetClients(ctx context.Context, u *unifi.Unifi) (float64, error) {
-	ss, err := u.GetSites()
+	sites, err := u.GetSites()
 	if err != nil {
 		return 0, fmt.Errorf("get sites: %w", err)
 	}
 
-	clients, err := u.GetClients(ss)
+	clients, err := u.GetClients(sites)
 	if err != nil {
 		return 0, fmt.Errorf("get clients: %w", err)
 	}
 
 	return float64(len(clients)), nil
+}
+
+type NetworkNumber struct {
+	Upload   float64
+	Download float64
+}
+
+// GetPacketsPerMinute returns the current rate of ppm from the WAN.
+func GetPacketsPerMinute(ctx context.Context, u *unifi.Unifi) (*NetworkNumber, error) {
+	sites, err := u.GetSites()
+	if err != nil {
+		return nil, fmt.Errorf("get sites: %w", err)
+	}
+
+	devs, err := u.GetDevices(sites)
+	if err != nil {
+		return nil, fmt.Errorf("get devices: %w", err)
+	}
+
+	data := &NetworkNumber{}
+	for _, d := range devs.UDMs {
+		// TODO: Maybe get this from WAN data instead?
+		data.Upload += d.TxBytes.Val
+		data.Download += d.RxBytes.Val
+	}
+
+	return data, nil
 }
